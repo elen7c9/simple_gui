dc791b0b34679db5e821a5de85325aa040675a8f
elen7c9 <enasiyati@gmail.com>
2018-04-26 22:02:44 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        SimpleGUI app = new SimpleGUI();
+        app.setVisible(true);
+    }
+}
